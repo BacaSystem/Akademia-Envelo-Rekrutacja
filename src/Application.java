76731c50cf9e95056d02405e264a128c20f6798e
Dominik Baczyński <dombacy@gmail.com>
2022-09-30 14:50:02 +0200

    zadanie 3+

--- src/Application.java
+++ src/Application.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
+
 public class Application {
+
+    ClientHttp clientHttp = new ClientHttp();
+    ArrayList<String> quotes = new ArrayList<String>();
+
     public void run() {
 
-        ClientHttp clientHttp = new ClientHttp();
         String cmd = "";
 
         System.out.println("#####################################################################################");
         System.out.println("Kanye West quotes.\nType 'next' to get another citation. Type 'exit' to exit program.");
         System.out.println("#####################################################################################");
+        
         String response = clientHttp.Get("https://api.kanye.rest/text");
+        if(response == null)
+            return;
+        quotes.add(response);
         System.out.println(response);
 
         while(cmd != "exit") {
@@ -18,6 +27,11 @@ public class Application {
                     response = clientHttp.Get("https://api.kanye.rest/text");
                     if(response == null)
                         return;
+                    if(quotes.contains(response)) {
+                        System.out.println("This quote was already shown. Try next one!");
+                        break;
+                    }
+                    quotes.add(response);
                     System.out.println(response);
                     break;
                 case "exit":
